c44996af4eb3e09059dda9831db9df6b87fe2006
Erewerere <parad0304@gmail.com>
2021-11-15 11:15:19 +0200

    notebook 15.11.2021

---
+++ Coursach/Coursach/Design.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coursach
+{
+    public partial class Design : Form
+    {
+        public Design()
+        {
+            InitializeComponent();
+        }
+
+        private void Design_Load(object sender, EventArgs e)
+        {
+            panel1.BackColor = ColorTranslator.FromHtml("#16024F");
+            this.BackColor = ColorTranslator.FromHtml("#DAC6C6");
+        }
+    }
+}
